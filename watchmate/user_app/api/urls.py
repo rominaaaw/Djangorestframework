4805db5dc023ee572fe1ac66a5758e5076830c4b
Romina Nakhaee <77361415+rominaaaw@users.noreply.github.com>
2022-01-15 18:56:39 +0330

    Add files via upload

---
+++ watchmate/user_app/api/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
+from user_app.api.views import Registration_view , Logout_view
+from rest_framework_simplejwt.views import (TokenObtainPairView,TokenRefreshView,)
+
+
+urlpatterns = [
+    path('login/', obtain_auth_token, name='login'),
+    path('register/', Registration_view, name='register'),
+    path('logout/',Logout_view, name='logout'),
+    
+
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+]
